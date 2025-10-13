4c7aeddfac7c01cc01a01cbb84fb3f0b118d9e60
pZiebell <philipp.ziebell.uni@gmail.com>
2025-10-13 11:50:01 +0200

    remove testing

--- src/main/java/com/example/BerlinerKollektivRanking/BerlinerKollektivRankingApplication.java
+++ src/main/java/com/example/BerlinerKollektivRanking/BerlinerKollektivRankingApplication.java
@@ -9,7 +9,6 @@ import java.util.List;
 @SpringBootApplication
 public class BerlinerKollektivRankingApplication {
 
-	//test
 	public static List<Kollektiv> kollektivList;
 	List<Dj> djList = Arrays.asList();
 
