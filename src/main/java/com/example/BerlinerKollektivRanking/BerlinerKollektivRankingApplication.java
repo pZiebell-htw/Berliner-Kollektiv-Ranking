0df4cdac5531eeec4f191e3acfa862bbe71c8419
pZiebell <philipp.ziebell.uni@gmail.com>
2025-10-13 02:15:01 +0200

    Spring-Projekt vom Spring Initializr hinzugefügt

---
+++ src/main/java/com/example/BerlinerKollektivRanking/BerlinerKollektivRankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BerlinerKollektivRanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinerKollektivRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerlinerKollektivRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BerlinerKollektivRanking/BerlinerKollektivRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BerlinerKollektivRanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinerKollektivRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
