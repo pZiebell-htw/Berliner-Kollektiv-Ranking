36361dbce41611b0906e38110f12a5f806da21c8
thomasfranznguyen <s0597417@htw-berlin.de>
2025-11-12 16:28:56 +0100

    milestone 3 test

---
+++ src/main/java/com/example/BerlinerKollektivRanking/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.BerlinerKollektivRanking;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://berliner-kollektiv-ranking-frontend.onrender.com/")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowCredentials(true);
+        }
+    }
